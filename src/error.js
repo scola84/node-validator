d5f713be1fd0eb9e5cae1257c18cc8cc34b0122b
M.C. van den Boogaart <scola@kvdb.name>
2016-08-02 18:14:23 +0200

    Use prefix instead of callback, split methods

--- src/error.js
+++ src/error.js
@@ -14,26 +14,21 @@ export default class ValidatorError extends Error {
     return this._errors;
   }
 
-  toString(i18n, callback) {
+  toString(i18n, fieldPrefix) {
     if (typeof i18n === 'undefined') {
       return this._raw();
     }
 
-    return this._format(i18n, callback);
+    return this._format(i18n, fieldPrefix);
   }
 
   _raw() {
-    let reason = null;
-    let value = null;
-
     return Object.keys(this._errors).map((field) => {
-      reason = Object.keys(this._errors[field]).pop();
-      value = JSON.stringify(this._errors[field][reason]);
-      return field + '=' + reason + ':' + value;
+      this._rawError(field);
     }).join('&');
   }
 
-  _format(i18n, callback) {
+  _format(i18n, fieldPrefix) {
     const prefix = 'scola.validator.';
     const string = i18n.string();
 
@@ -41,36 +36,42 @@ export default class ValidatorError extends Error {
       string.data(data);
     }
 
-    let text = '';
-    let error = null;
-    let type = null;
-    let detail = null;
-
     return Object.keys(this._errors).map((field) => {
-      text = '';
-      error = this._errors[field];
-
-      text += string.format(prefix + 'field.begin', {
-        field: callback ? callback(field) : field
-      });
-
-      type = Object.keys(error).pop();
-      text += string.format(prefix + 'field.' + type);
-
-      if (typeof error[type] === 'string') {
-        text += string.format(prefix + 'check.' +
-          type + '.' + error[type]);
-      }
-
-      if (typeof error[type] === 'object') {
-        detail = Object.keys(error[type]).pop();
-        text += string.format(prefix + 'check.' +
-          type + '.' + detail, error[type]);
-      }
-
-      text += string.format(prefix + 'field.end');
-
-      return text;
+      return this._formatError(string, field, prefix, fieldPrefix);
     }).join(' ');
+  }
+
+  _rawError(field) {
+    const reason = Object.keys(this._errors[field]).pop();
+    const value = JSON.stringify(this._errors[field][reason]);
+
+    return field + '=' + reason + ':' + value;
+  }
+
+  _formatError(string, field, prefix, fieldPrefix) {
+    let text = '';
+    const error = this._errors[field];
+
+    text += string.format(prefix + 'field.begin', {
+      field: string.format(fieldPrefix + field)
+    });
+
+    const type = Object.keys(error).pop();
+    text += string.format(prefix + 'field.' + type);
+
+    if (typeof error[type] === 'string') {
+      text += string.format(prefix + 'check.' +
+        type + '.' + error[type]);
+    }
+
+    if (typeof error[type] === 'object') {
+      const detail = Object.keys(error[type]).pop();
+      text += string.format(prefix + 'check.' +
+        type + '.' + detail, error[type]);
+    }
+
+    text += string.format(prefix + 'field.end');
+
+    return text;
   }
 }
