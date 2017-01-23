5aa95125308d8510fd069bbb668ef7ad80e7c677
M.C. van den Boogaart <scola@kvdb.name>
2017-01-23 17:08:06 +0100

    Load on app

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import strings from './src/i18n/strings';
 
 export { default as Validator } from './src/validator';
 
-export function load(i18n) {
-  i18n.strings(strings);
+export function load(app) {
+  app.i18n().strings(strings);
 }
