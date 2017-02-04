118c229d2b84f688f8797b216e18ce4b6ebe7ea4
M.C. van den Boogaart <scola@kvdb.name>
2017-02-04 13:32:31 +0100

    Only set strings when i18n is set

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ import strings from './src/i18n/strings';
 export { default as Validator } from './src/validator';
 
 export function load(app) {
-  app.i18n().strings(strings);
+  if (app.i18n()) {
+    app.i18n().strings(strings);
+  }
 }
