e43415c6ce8db8bf42f0608eef61b6304eb9ac31
M.C. van den Boogaart <scola@kvdb.name>
2017-03-03 21:31:31 +0100

    Refactor exports

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+import Validator from './src/validator';
 import strings from './src/i18n/strings';
 
-export { default as Validator } from './src/validator';
-
-export function load(app) {
+function load(app) {
   if (app.i18n()) {
     app.i18n().strings(strings);
   }
 }
+
+export {
+  Validator,
+  load
+};
